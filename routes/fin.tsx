60c6ff42ffb092fb2bd98c47ef2d4573f8926cc8
jonasarce1 <jonasarceg@gmail.com>
2024-03-08 19:54:17 +0100

    fin

---
+++ routes/fin.tsx
@@ -0,0 +1,9 @@
+
+export default function Page(){
+    return(
+        <div>
+            <h1>Fin del formulario, bien hecho</h1>
+            <a href="/">Volver a la pagina principal</a>
+        </div>
+    )
+}
